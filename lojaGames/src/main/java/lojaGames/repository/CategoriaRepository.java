fe7d98f1843df7b528fc92d6b5184ba870b7d00f
fabianagsilva <bisilvag2@gmail.com>
2022-10-14 00:38:33 -0300

    Task Loja de Games [Criando CRUD completo] 14.10.22

---
+++ lojaGames/src/main/java/lojaGames/repository/CategoriaRepository.java
@@ -0,0 +1,18 @@
+package lojaGames.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import lojaGames.model.Categoria;
+
+/**
+ * @author Fabiana
+ * @version 1.0
+ */
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+	public List<Categoria> findAllByTipoContainingIgnoreCase(String tipo);
+
+}
